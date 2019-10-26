0acd7c1d44e486856333894ff2653cc44351e14e
Niko Kaartinen <niko.kaartinen@protonmail.com>
2019-10-26 18:23:26 +0300

    Add more features for !slap functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Discord from 'discord.js';
 import config from './config';
+import slap from './slap';
 
 const bot = new Discord.Client();
 
@@ -9,14 +10,8 @@ bot.on('ready', () => {
 	console.log(`Logged in with ${bot.user.tag} as ${bot.user.username}!`); // eslint-disable-line no-console
 });
 
-/* Listen to messages */
 bot.on('message', msg => {
 	if (msg.content.startsWith('!slap')) {
-		const user = msg.mentions.users.first();
-		if (!user) {
-			msg.channel.send('No such user');
-		} else {
-			msg.channel.send(`${msg.author.username} slaps ${user.username} around a bit with a large trout`);
-		}
+		slap(msg);
 	}
 });
---
+++ src/slap/index.js
@@ -0,0 +1,17 @@
+export default function slap(msg) {
+	const admins = ['GicGer', 'Galloglaich'];
+
+	const user = msg.mentions.users.first();
+
+	if (!user) {
+		msg.channel.send('No such user');
+	} else {
+		if (msg.author.username === user.username) {
+			msg.channel.send('Stop hitting yourself');
+		} else if (admins.includes(user.username)) {
+			msg.channel.send(`${user.username} is a fine fellow!`);
+		} else {
+			msg.channel.send(`${msg.author.username} slaps ${user.username} around a bit with a large trout`);
+		}
+	}
+}
