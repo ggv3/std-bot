d09615e19ce1e9dac7c8ec5cf9b5326c3fa67555
Niko Kaartinen <niko.kaartinen@protonmail.com>
2019-12-08 11:43:48 +0200

    Add eslint and prettier config and prettify files

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const bot = new Discord.Client();
 bot.login(config.token);
 
 bot.on('ready', () => {
-	console.log(`Logged in with ${bot.user.tag} as ${bot.user.username}!`); // eslint-disable-line no-console
+  console.log(`Logged in with ${bot.user.tag} as ${bot.user.username}!`); // eslint-disable-line no-console
 });
 
 bot.on('message', msg => {
-	if (msg.content.startsWith('!slap')) {
-		slap(msg);
-	} else if (msg.content.startsWith('!kassa')) {
-		msg.channel.send(`Mee nyt vittuun ${config.tuukka}`);
-	}
+  if (msg.content.startsWith('!slap')) {
+    slap(msg);
+  } else if (msg.content.startsWith('!kassa')) {
+    msg.channel.send(`Mee nyt vittuun ${config.tuukka}`);
+  }
 });
--- src/slap/index.js
+++ src/slap/index.js
@@ -1,17 +1,19 @@
 export default function slap(msg) {
-	const admins = ['GicGer', 'Galloglaich'];
+  const admins = ['GicGer', 'Galloglaich'];
 
-	const user = msg.mentions.users.first();
+  const user = msg.mentions.users.first();
 
-	if (!user) {
-		msg.channel.send('No such user');
-	} else {
-		if (msg.author.username === user.username) {
-			msg.channel.send('Stop hitting yourself');
-		} else if (admins.includes(user.username)) {
-			msg.channel.send(`${user.username} is a fine fellow!`);
-		} else {
-			msg.channel.send(`${msg.author.username} slaps ${user.username} around a bit with a large trout`);
-		}
-	}
+  if (!user) {
+    msg.channel.send('No such user');
+  } else {
+    if (msg.author.username === user.username) {
+      msg.channel.send('Stop hitting yourself');
+    } else if (admins.includes(user.username)) {
+      msg.channel.send(`${user.username} is a fine fellow!`);
+    } else {
+      msg.channel.send(
+        `${msg.author.username} slaps ${user.username} around a bit with a large trout`,
+      );
+    }
+  }
 }
