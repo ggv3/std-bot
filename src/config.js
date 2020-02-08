3d513199a6e0459cd2e794b6c3405163bb673f38
Niko Kaartinen <niko.kaartinen@protonmail.com>
2020-02-08 19:42:47 +0200

    Create example config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  token: '{token}',
+  tuukka: '{correct emote id}',
+  endpoint: '{feedbackService endpoint}',
+  feedbackChannelId: '{channelId for feedbacks}',
+  roles: {
+    admin: '{admin role Id}',
+    moderator: '{moderator role Id}',
+  },
+};
