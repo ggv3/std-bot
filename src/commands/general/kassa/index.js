d344799ec73f14f755b779e20f02d4c8b81cb82b
Niko Kaartinen <niko.kaartinen@protonmail.com>
2021-03-06 12:22:14 +0200

    feat: add a feature to express massive disappointment to the situation

---
+++ src/commands/general/kassa/index.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'kassa',
+  aliases: ['kassa'],
+  usage: '!kassa',
+  description: 'Express massive disappointment',
+  execute: ({ msg }) => {
+    const {
+      channel = {},
+      guild: { emojis = {}, channels: { cache: channelCache } = {} } = {},
+      author,
+    } = msg || {};
+    if (channel.type === 'dm') return;
+    msg.channel.send(`Mee nyt vittuun ${emojis.cache.find(e => e.name === 'STDTuukka')}`);
+    channelCache
+      .get(process.env.BOT_SPAM_CHANNEL_ID)
+      .send(`User: ${author.username} used command: !slap`);
+  },
+};
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,4 +1,5 @@
 import slap from './general/slap';
 import feedback from './general/feedback';
+import kassa from './general/kassa';
 
-export default [slap, feedback];
+export default [slap, feedback, kassa];
